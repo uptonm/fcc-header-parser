589e5008252ca03893469bc200aff240e08d677f
Michael Upton <uptonm@wit.edu>
2019-02-25 08:16:54 -0500

    Init commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+// const subdomain = require('express-subdomain');
+const requestIP = require('request-ip');
+const morgan = require('morgan');
+// const path = require('path');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.set('trust proxy', 'loopback'); // specify a single subnet
+// app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(subdomain('api', app));
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/api/whoami', (req, res) => {
+  res.status(200).send(req.headers);
+});
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/client/build/index.html'));
+// });
+
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}`);
+});
